aa55d9ca3fd81daff5b6c22bee4d776951418f39
Sluchevska <alaska3a@gmail.com>
2021-07-25 23:23:50 +0100

    update

--- src/gallery-api.js
+++ src/gallery-api.js
@@ -10,7 +10,7 @@ export default class GalleryApiService {
         
         const baseUrl = 'https://pixabay.com/api/'
         const key = '22597300-51a9bfff07e627635843c3062'
-        const url = `${baseUrl}?key=${key}&q=${this.searchQuery}&image_type=photo&orintation=horizontal&safesearch=true&page=${this.page}&per_page=200`
+        const url = `${baseUrl}?key=${key}&q=${this.searchQuery}&image_type=photo&orintation=horizontal&safesearch=true&page=${this.page}&per_page=40`
         const response = await axios.get(url);
             // .then(response => response.json())
             
