38e681235cb4f82d240d7d591611e924c72e2a4c
Sluchevska <alaska3a@gmail.com>
2021-07-25 11:48:33 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './sass/main.scss';
 import { Notify } from 'notiflix';
+
+const url = 'https://pixabay.com/api/'
+const key = '22597300-51a9bfff07e627635843c3062'
+
+
+
+fetch(`${url}?key=${key}`)
+    .then(response = response.json())
+    .then(console.log);
